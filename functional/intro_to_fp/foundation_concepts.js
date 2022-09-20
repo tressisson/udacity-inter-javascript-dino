31be436b9b95f29121b47886b76fe3d46147f176
Tres Sisson <tressisson@gmail.com>
2022-09-20 16:27:50 -0400

    functional sections update

--- functional/intro_to_fp/foundation_concepts.js
+++ functional/intro_to_fp/foundation_concepts.js
@@ -1,24 +1,13 @@
-// Directions: Translate from normal function to pure function
-let greeting = 'Hello, '
-
-const greet = (name) => {
-    return greeting += name
+const greet = (name, base_greeting) => {
+    return `${base_greeting} ${name}`
 }
 
-// This code looks fine at first, but what if we needed to use this greet function again? 
-// The greet function has unexpected results if used more than once because the greeting 
-// variable is changed (mutated) to contain the name it is run with.
+let greeting = 'Hello, '
 
-// Here is the problem:
-console.log(greet('Arthur.'))
-console.log(greet('Guinevere.'))
-// Output: 
+greet('Arthur.', greeting)
+// now we can reuse greeting, and the greet function is pure
+
+console.log(greet('Arthur.', greeting)) 
+// expected output: 
 //  Hello, Arthur. 
-//  Hello, Arthur. Guinevere. 
-
-// But that isn't what we want! There are many many ways you can fix this 
-// function to make it pure. When it is pure it will also be resuable.
-
-// Desired output:
-//  Hello, Arthur. 
-//  Hello, Guinevere. 
+//  Hello, Arthur.
